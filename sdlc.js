24a04ccda0ccaa309e5a1441b9e20c2d7ffb7e9e
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-08-19 10:15:45 -0600

    sdlc completed

--- sdlc.js
+++ sdlc.js
@@ -12,10 +12,24 @@
 // Testing & Integration
 // Planning
 
+// MY ANSWER VVV
+
+// Planning
+// Analysis of requirments
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
+
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirments = "Figuring out the requirements for the project by asking things like; who will use the software, how will it be used, what kind of data should be input and output."
+const Design = "Design takes place based on what was determined in the Analysis of Requirments phase."
+const Implementation = "Developers begin coding."
+const testingAndIntegration = "Testing the sofware to be sure it is functioning as it should and meating the determined requirments."
+const Maintain = "As software is put to use, problems and chnages will occur that need attention."
