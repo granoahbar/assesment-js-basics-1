8b219a7f0d3f98d41ccbdbcd3cb18b969df9b0a0
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-08-19 10:56:13 -0600

    arrays completed

--- arrays.js
+++ arrays.js
@@ -3,12 +3,16 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+let colorCopy = faveColors.slice (0, 3)
+
+console.log (colorCopy)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push ('blue')
+
+console.log (colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +22,9 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+let middleNums = numbers.slice (1, 4)
+
+console.log (middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -33,4 +39,17 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+let answers = []
+
+for (i = 0; i <= bigOrSmallArray.length; i++)
+{
+  if (bigOrSmallArray[i] > 100)
+  {
+    answers.push ('big')
+  }
+  else if (bigOrSmallArray[i] <= 100)
+  {
+    answers.push ('small')
+  }
+}
+console.log (answers)
